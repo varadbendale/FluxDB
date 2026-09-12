d515252e023fc16845bd59ade93807c4e66ed5e6
Varad <varadbendale17@gmail.com>
2026-09-12 23:02:29 +0530

    Data added in columns
    from here the unique and the striking of stuff as per the size given needs to be worked on

--- src/Storage Engine/File/Pages/Insert_pager.c
+++ src/Storage Engine/File/Pages/Insert_pager.c
@@ -43,19 +43,21 @@ struct_for_insert(itree * tree ){
     int heck = 0 ; 
     int hash_1[307] ; 
     int hash_2[307] ; 
-    int position[307] ; 
+    int position_org[307] ; 
+    int position_written[307] ; 
     if (table->table_col_num > 0 ){
         int temp_1 = 0 ; 
         int temp_2 = 0 ; 
         while ( temp_1 < temp_table.num_columns ){
             int num = hash_num(temp_table.columns[temp_1].col_name) ; 
             hash_1[num] = num ; 
+            position_org[num] = temp_1 ; 
             temp_1++ ;    
         }
         while ( temp_2 < table->table_col_num ){
             int num = hash_num( table->table_cols[temp_2]) ; 
             position[num] = temp_2 ; 
-            hash_2[num] = num ; 
+            position_written[temp_2] = num ; 
             temp_2++ ;    
         }
         temp_1 = 0 ; 
@@ -78,6 +80,28 @@ struct_for_insert(itree * tree ){
             }
         }
 
+    }
+    char ** col_temp = calloc(128, sizeof(char *)); 
+    int d = 0  ; 
+    int counter = 0 ; 
+    while ( d < tree->num ){
+        if (strcmp(tree->children[d]->comp ,"VALUE_ROW" ) == 0 ){
+            itree * temp_tree = tree->children[d] ; 
+            int e = 0 ; 
+            int col_num = 0 ; 
+            while (e < temp_tree->num){
+                col_temp[hash_1[hash_2[hash_num(temp_tree->children[e]->comp)]]] = strdup(temp_tree->children[e]->comp) ; 
+                e++ ; 
+            }
+            int f = 0 ; 
+            while ( f < temp_table.num_columns){
+                if (col_temp[f] == NULL ){
+                   col_temp[f] = temp_column[counter++] ; 
+                }
+                f++ ; 
+            }
+        }
+        d++ ; 
     }
 
 
