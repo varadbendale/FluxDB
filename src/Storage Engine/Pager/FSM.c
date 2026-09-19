d6b8e7ca0c254629a5586bca58b8042b632825b3
Varad <varadbendale17@gmail.com>
2026-09-19 16:42:33 +0530

    Till "need to rearrange space there "

--- src/Storage Engine/Pager/FSM.c
+++ src/Storage Engine/Pager/FSM.c
@@ -27,11 +27,14 @@ page *get_page_from_file( int offset , char *filename  ){
 
 
 
-int find_the_size_of_stuff(char ** data  , table_info * table  , char * ans  ){
+int find_the_size_of_stuff_get_the_data_and_get_the_primary_key_num(char ** data  , table_info * table  , char * ans  , float *pk  ){
     int size  = 0 ; 
     int i = 0 ;
-    int j = 0 ; 
+    int j = 0 ;      
     while ( i < table.num_columns){
+        if (table.columns[i].primary_key == true ){
+            pk = &(float)atof(table.columns[i].col_name ); 
+        }   
         if (table.columns[i].col_type == INT || table.columns[i].col_type == BIGINT  || table.columns[i].col_type == SMALLINT  || table.columns[i].col_type == TINYINT    ){
             size = size + strlen(table.columns[i].type_bits_count) ; 
             ans[j] = strdup(integer_num) ; 
@@ -100,22 +103,96 @@ pagezero *get_pagezero(char *filename) {
     return ans;
 }
 
-void FSM(char * data , int page_num , int offset , table_info * table ){
+
+/* 
+    int slots_figured_out(page * pg , float pk ){
+        int low  = 0 ; 
+        int high = pg->slot_num ; 
+        while ( low <= high ){
+            int mid = ( low + high ) / 2  ; 
+            if (pk < pg->slot[mid]->pk ){
+                high = mid - 1 ; 
+            }
+            else if (pk == pg->slot[mid]->pk  ){
+                return NULL ; 
+            }
+            else { 
+                low = mid + 1 ; 
+            }
+        }
+        return low ; 
+    } 
+*/
+
+for (int i = 7; i >= 0; i--) {
+    int bit = (val >> i) & 1;
+    printf("%d", bit);
+}
+
+void FSM(char * insert_data , int page_num , int offset , table_info * table ){
     page * pg  ; 
     char * ans ; 
     int size = 0 ; 
+    float * primary_key = 0 ; 
+    int first = 0 ; 
     if (offset > 0 ){
         pg = get_page_from_file(offset) ; 
         if (pg->header->page_num != page_num  ){
             return  ; 
         }
-        size = find_the_size_of_stuff( data  , table  , &ans ) ; 
+        size = find_the_size_of_stuff_get_the_data_and_get_the_primary_key_num( data  , table  , &ans , &primary_key ) ; 
+        size = size + sizeof(slots) ; 
         if (size < pg->header->free_size ){
             if (size < pg->header->normal_free_size){
                 slots * temp_slot = malloc(sizeof(slots)) ; 
+                int current_offset = pg->header->current_offset + pg->header->current_size
+                pg->data[current_offset] = strdup(ans) ; 
+                pg->slots[pg->slot_num]->size = size - sizeof(slots)  ; 
+                pg->slots[pg->slot_num]->offset = pg->current_offset + pg->slots[pg->slot_num]->size   ; 
+                pg->slots[pg->slot_num]->pk = *primary_key  ;
+                int temp_slot_num = pg->slot_num / 8 ; 
+                pg->header->dead_slots[temp_slot_num] = ( pg->header->dead_slots[temp_slot_num] << 1) | 1;  
+                pg->slot_num++ ; 
             }
             else { 
-                //rearrange ; 
+                int i = 0  ; 
+                int j = 0 ; 
+                int k = 0 ; 
+                int where_to_put = -1 ; 
+                int temp_slot_num = pg->slot_num / 8 ; 
+                int temp_mod = pg->slot_num % 8 ; 
+                uint8_t temp  ; 
+                while (i < temp_slot_num){
+                    temp = pg->header->dead_slots[i] ; 
+                    for (int j = 7; j >= 0; j--) {
+                        int bit = (temp >> j) & 1;
+                        if (bit == 0 ){
+                            where_to_put = (i*8) + j ;
+                            k = i  ; 
+                            if (size - sizeof(slots) > pg->slots[where_to_put]->size ) {
+                                where_to_put = -1 ; 
+                            }
+                            break ; 
+                        }
+                    }
+                    if (where_to_put != -1 ){
+                        break ; 
+                    }
+                }
+                if (where_to_put != -1 ){
+                    pg->header->free_size = pg->header->free_size + pg->slots[where_to_put]->size - (size - sizeof(slots)); 
+                    pg->slots[where_to_put]->size = size - sizeof(slots); 
+                    pg->slots[where_to_put]->pk = primary_key ; 
+                    free(pg->data[pg->slots[where_to_put]->offset]) ;
+                    pg->data[pg->slots[where_to_put]->offset] = ans ;
+                    int index = temp_mod ;  
+                    int bit_pos = 7 - index ;
+                    temp |= (1 << bit_pos) ;
+                    pg->header->dead_slots[k] = temp  ; 
+                }
+                else { 
+                    
+                }
             }
         }
         else { 
